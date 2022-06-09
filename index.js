b414e6466a68e3bf07b1bd35eaa72022c29e1b01
Holly <hollylovesapple@hotmail.com>
2022-06-09 21:25:12 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+let password = document.getElementById("password");
+
+let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@£$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let passwordLength = 12;
+//let password =""
