750882a82b071f30843167234096d564081120be
Holly <hollylovesapple@hotmail.com>
2022-10-13 15:33:45 +0100

    JS now working

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-let password = document.getElementById("password");
-
-let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@£$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let password = "";
+//password variable is blank
+let newarray = [];
+let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@£$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let passwordLength = 12;
 //let password =""
+
+for (i = 0; i < passwordLength; i++) {
+    let pass = Array.from(chars)
+    let randomElement = Array.from(pass[Math.floor(Math.random() * pass.length)]);
+    newarray.push(randomElement);
+}
+console.log(newarray.join(''));
